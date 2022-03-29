7212a5e7e1c3e318f46d9fcb7d1f6e8abdb20660
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-29 19:46:36 +0900

    Prepare Calendar

---
+++ Practice04.java
@@ -0,0 +1,35 @@
+package Mar29th;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Practice04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar.getTime());
+		System.out.println(calendar.get(Calendar.YEAR));
+		System.out.println(calendar.get(Calendar.MONTH));
+		System.out.println(calendar.get(Calendar.DATE));
+		System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+		System.out.println(calendar.get(Calendar.HOUR));
+		System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println(calendar.get(Calendar.MINUTE));
+		System.out.println(calendar.get(Calendar.SECOND));
+		System.out.println(calendar.get(Calendar.MILLISECOND));
+		
+		
+		// Operation oper = new Operation();
+		// Price pri = new Price();
+		/*
+		 * Date date = new Date(); System.out.println(date.toString());
+		 * System.out.println(date.getMonth() + 1); System.out.println(date.getHours());
+		 * System.out.println(date.getDay()); System.out.println(date.getTime());
+		 * System.out.println(date.getTimezoneOffset());
+		 */
+	}
+
+}
